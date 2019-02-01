6ed7f5d3eeca8ccf59a4b974c579bc2bc33acda4
Aman Jain <AmanJa@hcl.com>
2019-02-01 17:14:39 +0530

    initial commit

---
+++ src/Router.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import CarManagement from "./components/user/car-management/carManagement";
+import CarDetail from "./components/user/car-management/carDetail";
+
+const Router = () => (
+  <Switch>
+    <Route exact path="/" component={CarManagement} />
+    <Route exact path="/carDetails/:incidentId" component={CarDetail} />
+  </Switch>
+);
+
+export default Router;
